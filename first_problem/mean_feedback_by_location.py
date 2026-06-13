9fa08c89855d742cd95877eb823cd94ce3b5984d
NicolasTeles <94422997+NicolasTeles@users.noreply.github.com>
2026-06-13 17:14:55 -0300

    ta pronto

--- first_problem/mean_feedback_by_location.py
+++ first_problem/mean_feedback_by_location.py
@@ -1,5 +1,8 @@
 import csv
 from collections import defaultdict
+import numpy as np
+from heatmap_feedback import save_heatmap
+import matplotlib.pyplot as plt
 
 def mean_columns_by_location(csv_path):
 
@@ -11,7 +14,7 @@ def mean_columns_by_location(csv_path):
         result = {}
         
         for column_name in fieldnames:
-            if column_name == "time":
+            if column_name == "time" or column_name == "location":
                 continue
             totals = defaultdict(float)
             counts = defaultdict(int)
@@ -38,13 +41,25 @@ def mean_columns_by_location(csv_path):
             
         return result
 
+def convert_array_pos_into_matrix_pos(location):
+    return (location // 4, location % 4)
 
 if __name__ == '__main__':
     csv_file = 'mc1-reports-data.csv'
     result = mean_columns_by_location(csv_file)
 
-    for key, value in result.items():
-        print(f'Mean {key} by location:')
+    heat_dict = {}
+    for column_name, value in result.items():
+        heat_matrix = np.full((5, 4), np.nan)
+        print(f'Mean {column_name} by location:')
         for location, mean_column in sorted(value.items(), key=lambda item: int(item[0])):
             print(f'Location {location}: {mean_column:.4f}')
+            
+            i, j = convert_array_pos_into_matrix_pos(int(location)-1)
+            heat_matrix[i][j] = mean_column
         print()
+        heat_dict[column_name] = heat_matrix
+        
+    for column, matrix in heat_dict.items():
+        save_heatmap(column, matrix, heat_dict["shake_intensity"])
+        plt.clf()
